50c61a118b33a31a9239c8b1698b8056f4389b7a
hiltonfarias <hilton.farias.da.silva@gmail.com>
2021-03-04 00:52:57 -0300

    implementing get by id method

--- src/main/java/com/github/hiltonfarias/citiesapi/CountryResource.java
+++ src/main/java/com/github/hiltonfarias/citiesapi/CountryResource.java
@@ -1,13 +1,15 @@
 package com.github.hiltonfarias.citiesapi;
 
 import com.github.hiltonfarias.citiesapi.countries.Country;
+import com.github.hiltonfarias.citiesapi.exception.BadRequestException;
 import com.github.hiltonfarias.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/countries")
@@ -17,7 +19,16 @@ public class CountryResource {
     private CountryRepository countryRepository;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Page<Country> countries(Pageable pageable) {
         return countryRepository.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Country> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(countryRepository
+                .findById(id)
+                .orElseThrow(() -> new BadRequestException("Country id not found")));
     }
 }
